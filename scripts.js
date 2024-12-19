a7963f51d77e573922be618408d4ca85d973aaa7
Pjotters <05.hek_domein@icloud.com>
2024-12-19 16:51:05 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
-// Voeg hier je JavaScript toe voor interacties of animaties
 document.addEventListener('DOMContentLoaded', function() {
     // Voorbeeld: klik op de downloadknop
-    const downloadButtons = document.querySelectorAll('.download-button');
+    const downloadButtons = document.querySelectorAll('.cta-button');
     downloadButtons.forEach(button => {
         button.addEventListener('click', function(event) {
             alert('Je app wordt gedownload!');
